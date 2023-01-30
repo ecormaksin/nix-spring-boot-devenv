77b618918cdf8c83c086138f7e926de4e88ecaab
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2023-01-31 00:57:11 +0900

    add how to setup docker and launch intellij idea

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
   1. [Install Nix](https://nixos.org/download.html)
   2. Execute `echo "{ allowUnfree = true; }" > ~/.config/nixpkgs/config.nix`
   3. [Install direnv](https://nixos.org/guides/declarative-and-reproducible-developer-environments.html)
+  4. Setup docker
+
+    ```bash
+     sudo ln -s /nix/store/<installed-folder>/etc/systemd/system/docker.service /usr/lib/systemd/system/docker.socket
+     sudo ln -s /nix/store/<installed-folder>/etc/systemd/system/docker.socket /usr/lib/systemd/system/docker.socket
+     sudo systemctl enable docker
+     sudo groupadd docker
+     sudo usermod -aG docker $USER
+     sudo chown $USER:$USER /run/docker.sock
+     sudo systemctl start docker
+     ```
+
+- Tips
+  - [How to launch Intellij IDEA](https://stackoverflow.com/questions/63889372/launching-ide-directly-from-nix-shell)
+    - `idea-community 2>1 > /dev/null &`
--- shell.nix
+++ shell.nix
@@ -1,36 +1,42 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cdead16a444a3e5de7bc9b0af8e198b11bb01804.tar.gz") {} }:
 pkgs.mkShell {
-  packages =
-  [
-    pkgs.checkstyle
-    pkgs.curl
-    pkgs.docker
-    pkgs.docker-compose
-    pkgs.gradle
-    pkgs.gradle-completion
-    pkgs.jdk
-    pkgs.jetbrains.idea-community
-    pkgs.jq
-    pkgs.lombok
-    pkgs.nodejs-18_x
-    pkgs.powershell
-    pkgs.tmux
-    pkgs.vscode
-    pkgs.vscode-extensions.alefragnani.bookmarks
-    pkgs.vscode-extensions.alexdima.copy-relative-path
-    pkgs.vscode-extensions.asciidoctor.asciidoctor-vscode
-    pkgs.vscode-extensions.editorconfig.editorconfig
-    pkgs.vscode-extensions.esbenp.prettier-vscode
-    pkgs.vscode-extensions.mikestead.dotenv
-    pkgs.vscode-extensions.ms-ceintl.vscode-language-pack-ja
-    pkgs.vscode-extensions.ms-vscode.PowerShell
-    pkgs.vscode-extensions.oderwat.indent-rainbow
-    pkgs.vscode-extensions.redhat.java
-    pkgs.vscode-extensions.vscjava.vscode-java-debug
-    pkgs.vscode-extensions.vscjava.vscode-java-test
-    pkgs.vscode-extensions.vscjava.vscode-maven
-    pkgs.vscode-extensions.vscjava.vscode-spring-initializr
-    pkgs.vscode-extensions.yzhang.markdown-all-in-one
-    pkgs.wget
+  packages = with pkgs; [
+    checkstyle
+    curl
+    docker
+    docker-compose
+    glibcLocales
+    gradle
+    gradle-completion
+    jdk
+    jetbrains.idea-community
+    jq
+    lombok
+    nodejs-18_x
+    powershell
+    tmux
+    vscode
+    vscode-extensions.alefragnani.bookmarks
+    vscode-extensions.alexdima.copy-relative-path
+    vscode-extensions.asciidoctor.asciidoctor-vscode
+    vscode-extensions.editorconfig.editorconfig
+    vscode-extensions.esbenp.prettier-vscode
+    vscode-extensions.mikestead.dotenv
+    vscode-extensions.ms-ceintl.vscode-language-pack-ja
+    vscode-extensions.ms-vscode.PowerShell
+    vscode-extensions.oderwat.indent-rainbow
+    vscode-extensions.redhat.java
+    vscode-extensions.vscjava.vscode-java-debug
+    vscode-extensions.vscjava.vscode-java-test
+    vscode-extensions.vscjava.vscode-maven
+    vscode-extensions.vscjava.vscode-spring-initializr
+    vscode-extensions.yzhang.markdown-all-in-one
+    wget
   ];
+
+  LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
+
+  LANGUAGE = "ja_JP.UTF-8";
+  LANG     = "ja_JP.UTF-8";
+  LC_ALL   = "ja_JP.UTF-8";
 }
